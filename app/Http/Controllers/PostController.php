79e03238bee0353326289942972c2ff1f38daf74
rizky ferdian <rizkyferdian002@gmail.com>
2022-09-12 03:40:51 +0700

    Menambah fitur upload image & tambah data

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -13,4 +13,32 @@ class PostController extends Controller
         $posts = Post::latest()->paginate(5);
         return view('posts.index', compact('posts'));
     }
+
+    public function create()
+    {
+        return view('posts.create');
+    }
+    public function store(Request $request)
+    {
+        //Validasi Form
+        $this->validate($request, [
+            'image'     => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'title'     => 'required|min:5',
+            'content'   => 'required|min:10'
+        ]);
+
+        //Upload Image
+        $image = $request->file('image');
+        $image->storeAs('public/posts', $image->hashName());
+
+        //Create Post
+        Post::create([
+            'image' => $image->hashName(),
+            'title' => $request->title,
+            'content' => $request->content,
+        ]);
+
+        //redirect to index
+        return redirect()->route('posts.index')->with(['success' => 'Data berjasi ditambahkan!']);
+    }
 }
